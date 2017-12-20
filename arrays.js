063a206ae7b46509cc506d4ccddd0a30ce667f71
Monica Tamaru Jones <mntamaru@gmail.com>
2017-12-20 20:53:16 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,2 +1,17 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
+function addElementToBeginningOfArray(arr, element) {
+  return [element, ...arr]
+}
+
+function destructivelyAddElementToBeginningOfArray(arr, element) {
+  return arr.unshift(element)
+}
+
+function addElementToBeginningOfArray(arr, element) {
+  return [element, ...arr]
+}
+
+function destructivelyAddElementToBeginningOfArray(arr, element) {
+  return arr.unshift(element)
+}

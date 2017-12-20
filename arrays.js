dd6bcea63e95ed5929f53765146cba2b9fab304f
Monica Tamaru Jones <mntamaru@gmail.com>
2017-12-20 20:57:20 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -16,3 +16,8 @@ function destructivelyAddElementToEndOfArray(arr, element) {
   return arr.push(element)
 }
 
+function accessElementInArray(arr, index) {
+  return arr[index]
+}
+
+function destructivelyAddElementToEndOfArray
